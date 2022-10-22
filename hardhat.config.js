7243c445265818845e62b0000990dface5d446e0
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-10-22 12:22:28 +0100

    setup

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+console.log(PRIVATE_KEY);
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: API_URL,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  },
+}
