9e844b85b0a63c4105e4b6d0d71ad9629b091fa0
meyad-g <79023203+meyad-g@users.noreply.github.com>
2022-10-22 23:18:25 +0100

    create stock request token

---
+++ contracts/StockRequestNFT.sol
@@ -0,0 +1,87 @@
+// Contract based on https://docs.openzeppelin.com/contracts/4.x/erc721
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract StockRequestNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address public owner;
+
+    constructor() ERC721("StockRequestNFT", "SFT") {
+        owner = msg.sender;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // Modifier to check that the caller is the owner of
+    // the contract.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    // This modifier checks that the
+    // address passed in is not the zero address.
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    // Modifiers can be called before and / or after a function.
+    // This modifier prevents a function from being called while
+    // it is still executing.
+    modifier noReentrancy() {
+        require(!locked, "No reentrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        validAddress(recipient)
+        noReentrancy
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function burnNFT(uint256 tokenId)
+        public
+        onlyOwner
+        validAddress(msg.sender)
+        noReentrancy
+    {
+        _burn(tokenId);
+    }
+}
+
+// how to delete metdata from nft storage using client.delete
--- scripts/store-assets.mjs
+++ scripts/store-assets.mjs
@@ -4,9 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const { NFT_STORAGE_API_KEY } = process.env
+const client = new NFTStorage({ token: NFT_STORAGE_API_KEY })
 
 async function storePrescription() {
-    const client = new NFTStorage({ token: NFT_STORAGE_API_KEY })
     const metadata = await client.store({
         name: 'ExampleNFT',
         description: 'My ExampleNFT is an awesome artwork!',
@@ -18,6 +18,21 @@ async function storePrescription() {
     })
     console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url)
 }
+
+async function storeStockRequest() {
+    const metadata = await client.store({
+        drug_info: {
+            drug_id: '1',
+            drug_quantity: '2',
+            drug_dosage: '3',
+            drug_frequency: '4',
+            drug_duration: '5',
+        },
+        prescription_id: '1',
+    })
+    console.log("Stock request metdata stored on Filecoin and IPFS with URL:", metadata.url)
+}
+
 
 storeAsset()
     .then(() => process.exit(0))
